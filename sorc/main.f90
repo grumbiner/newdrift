87ffcb667aa132c6b107f20a4975947a91ba6910
Robert Grumbine <grumbinespam@aol.com>
2022-02-02 08:01:01 -0500

    separating towards subroutine version

--- sorc/main.f90
+++ sorc/main.f90
@@ -11,7 +11,8 @@ PROGRAM newdrift
   PARAMETER (nvar = 24)
   PARAMETER (nvar_out = 6)
   INTEGER ncid, varid(nvar)
-  INTEGER ncid_out, varid_out(nvar_out)
+  INTEGER ncid_out, ncid_drift
+  INTEGER varid_out(nvar_out), varid_driftic(nvar_out)
   INTEGER dimids(2)
 !
   INTEGER nx, ny
@@ -34,22 +35,32 @@ PROGRAM newdrift
 !For drifter:
   CLASS(drifter), allocatable :: buoys(:,:)
 !
-  CHARACTER(90) fname
+  CHARACTER(90) fname, drift_name, outname
 
-  
 
 ! Allocate space for variables and initialize the netcdf reading
   ALLOCATE(allvars(nx, ny, nvar))
   ALLOCATE(ulat(nx, ny), ulon(nx, ny), dx(nx, ny), dy(nx, ny), rot(nx, ny))
   ALLOCATE(u(nx, ny), v(nx, ny))
 
+! Forcing / velocities
   fname = "cice.nc"
   CALL initialize_in(fname, ncid, varid)
+  !Get first set of data and construct the local metric for drifting
+  CALL read(nx, ny, nvar, ncid, varid, allvars)
+  CALL local_metric(ulat, ulon, dx, dy, rot, nx, ny)
 
-  !----------- Initialize buoys, this should be a read in 
+! Initialize Output -- need definite sizes
+  outname = "output.nc"
   ratio = 5
   imax = INT(nx/ratio)
   jmax = INT(ny/ratio)
+  CALL initialize_out(outname, ncid_out, varid_out, nvar_out, imax, jmax, dimids)
+
+  !----------- Initialize buoys, this should be a read in 
+  drift_name = "drift_in.nc"
+!  CALL initialize_drifter(drift_name, ncid_drift, varid_driftin, nvar_out, buoys)
+!  CALL close_out(ncid_drift)
 !RG: go to 1d list of points
   ALLOCATE(buoys(imax,jmax))
   DO j = 1, jmax
@@ -64,10 +75,9 @@ PROGRAM newdrift
   ENDDO
 
 !----------------------------------------------------------------
+! sbr(buoys, u, v, dx, dy, dt, nx, ny, nstep, nvar, ncid, varid, allvars)
 
 ! First time step:
-  CALL read(nx, ny, nvar, ncid, varid, allvars)
-  CALL local_metric(ulat, ulon, dx, dy, rot, nx, ny)
   DO j = 1, jmax
   DO i = 1, imax
     CALL buoys(i,j)%move(u, v, dx, dy, dt, nx, ny)
@@ -87,9 +97,6 @@ PROGRAM newdrift
 
 !----------------------------------------------------------------
 ! Write out results -- drift distance and direction
-  fname = "output.nc"
-  CALL initialize_out(fname, ncid_out, varid_out, nvar_out, imax, jmax, dimids)
-
   CALL outvars(ncid_out, varid_out, nvar_out, buoys, imax, jmax)
 
   CALL close_out(ncid_out)
