349ba07a37f7dbc4eed87388a2a1707ddb6cb43f
Robert Grumbine <Robert.Grumbine@noaa.gov>
2023-09-15 15:11:41 -0400

    build on gaea-c5

--- sorc/makeall.sh
+++ sorc/makeall.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #modules :
-# wcoss2:
-module load PrgEnv-intel/8.3.3
-module load netcdf/4.7.4
-module load intel-classic/2022.2.0.262
+## wcoss2:
+#module load PrgEnv-intel/8.3.3
+#module load netcdf/4.7.4
+#module load intel-classic/2022.2.0.262
 
 # gaea:
-#module load intel-classic/2022.2.1
-#module load cray-hdf5/1.12.2.3 
-#module load cray-netcdf/4.9.0.3
-#export NETCDF=$NETCDF_DIR
+module load intel-classic/2022.2.1
+module load cray-hdf5/1.12.2.3 
+module load cray-netcdf/4.9.0.3
+export NETCDF=$NETCDF_DIR
 
 # hera:
 #module load hpc/1.2.0  hpc-intel/2022.1.2
