56c6c57b911cc5993beda712d64be6cb0f010737
Robert.Grumbine <grumbinespam@aol.com>
2024-11-08 06:48:55 -0500

    bring in lnx116 and try but fail to run on macos

--- sorc/makeall.sh
+++ sorc/makeall.sh
@@ -3,17 +3,17 @@
 #modules :
 
 ## wcoss2:
-module load PrgEnv-intel/8.3.3
-module load netcdf/4.7.4
-module load intel-classic/2022.2.0.262
-ln -sf mk.wcoss2 mk.this
+#module load PrgEnv-intel/8.3.3
+#module load netcdf/4.7.4
+#module load intel-classic/2022.2.0.262
+#ln -sf mk.wcoss2 mk.this
 
 ## gaea:
-#module load intel-classic/2022.2.1
-#module load cray-hdf5/1.12.2.3 
-#module load cray-netcdf/4.9.0.3
-#export NETCDF=$NETCDF_DIR
-#ln -sf mk.gaea mk.this
+module load intel-classic/2023.2.0
+module load cray-hdf5/1.12.2.11
+module load cray-netcdf/4.9.0.9
+export NETCDF=$NETCDF_DIR
+ln -sf mk.gaea mk.this
 
 ## hera:
 #module load hpc/1.2.0  hpc-intel/2022.1.2
